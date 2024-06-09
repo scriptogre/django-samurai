323bfa4fdafc4d997fd360c935df3462906b4fb0
Madalin Popa <contact@madalinpopa.com>
2024-06-09 13:33:07 +0200

    chore: Update file patterns tests (#2)

--- tests/demo_project/demo/urls.py
+++ tests/demo_project/demo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from samurai import file_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    *file_patterns("demo/views"),
 ]
