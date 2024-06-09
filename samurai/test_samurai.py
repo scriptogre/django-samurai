03634b8aab3a3f67c9f8e83f143abc53e0646c97
Madalin Popa <contact@madalinpopa.com>
2024-06-09 15:29:39 +0200

    feat: Add get_url function to extract URL from file path (#23)

--- samurai/test_samurai.py
+++ samurai/test_samurai.py
@@ -1,8 +1,8 @@
 import shutil
-
+import re
 import pytest
 
-from . import file_patterns, get_files, exclude_file, get_module_path
+from . import file_patterns, get_files, exclude_file, get_module_path, get_url
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -40,6 +40,14 @@ def test_get_module_path():
     assert "views.colors.add" == module
 
 
+def test_get_url():
+    """Test get url from file path"""
+    file = get_files("views")[1]
+    start_dir_re = re.compile("^views")
+    url = get_url(file, start_dir_re, append_slash=False, view_fn="view_fn")
+    assert url == "colors/add"
+
+
 def test_append_slash():
     patterns = file_patterns("views", append_slash=True, exclude="")
     output = [(str(p.pattern), p.name) for p in patterns]
@@ -55,7 +63,6 @@ def test_append_slash():
 def test_exclude_path():
     patterns = file_patterns("views", append_slash=False, exclude="*time.py")
     output = [(str(p.pattern), p.name) for p in patterns]
-    print(output)
     assert output == [
         ("colors/add", "colors_add"),
         ("colors", "colors"),
