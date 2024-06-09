e685b7b344f701a033d1e33a16670f975a3c5c52
Madalin Popa <contact@madalinpopa.com>
2024-06-09 13:57:28 +0200

    feat: Refactor file patterns generation logic and version update

--- samurai/__init__.py
+++ samurai/__init__.py
@@ -7,7 +7,7 @@ from django.template import RequestContext
 from django.template.base import Template
 from django.urls import path
 
-__version__ = "0.4.0"
+__version__ = "0.1.0"
 
 DISALLOWED_CHARS = re.compile(
     "|".join(
@@ -28,35 +28,83 @@ def file_patterns(start_dir: str, append_slash: bool = False, exclude: str = "")
     """
     patterns = []
     start_dir_re = re.compile(f"^{start_dir}")
-    files = pathlib.Path(start_dir).glob("**/*.py")
-    # Reverse-sort the list so files that start with "<" go to the bottom
-    # and regular files come to the top. This ensures hard-coded url params
-    # always match before variable ones like <pk> and <slug>
-    files = sorted(files, reverse=True, key=str)
+    files = get_files(start_dir, exclude)
+
     for file in files:
-        if exclude and pathlib.Path.match(file, exclude):
+        if exclude_file(file, exclude):
             continue
 
-        module_path = str(file).replace(".py", "").replace("/", ".")
+        module_path = get_module_path(file)
         module = import_module(module_path)
-        view_fn = getattr(module, "view", None)
-        if not callable(view_fn):
+        view_fn = get_view_fn(module)
+
+        if not is_callable(view_fn):
             continue
 
-        try:
-            url = view_fn.url
-        except AttributeError:
-            url = "" if file.name == "__init__.py" else file.name.replace(".py", "")
-            url = start_dir_re.sub("", f"{file.parent}/{url}").strip("/")
-            url = (url + "/") if append_slash and url != "" else url
-
-        try:
-            urlname = view_fn.urlname
-        except AttributeError:
-            urlname = DISALLOWED_CHARS.sub("", TO_UNDERSCORES.sub("_", url))
+        url = get_url(file, start_dir_re, append_slash, view_fn)
+        urlname = get_urlname(view_fn, url)
 
         patterns.append(path(url, view_fn, name=urlname))
+
     return patterns
+
+
+def get_files(start_dir: str, exclude: str):
+    """
+    Get a list of files in the directory structure
+    """
+    files = pathlib.Path(start_dir).glob("**/*.py")
+    return sorted(files, reverse=True, key=str)
+
+
+def exclude_file(file: pathlib.Path, exclude: str):
+    """
+    Check if a file should be excluded
+    """
+    return exclude and pathlib.Path.match(file, exclude)
+
+
+def get_module_path(file: pathlib.Path):
+    """
+    Get the module path from the file path
+    """
+    return str(file).replace(".py", "").replace("/", ".")
+
+
+def get_view_fn(module):
+    """
+    Get the view function from the module
+    """
+    return getattr(module, "view", None)
+
+
+def is_callable(fn):
+    """
+    Check if a function is callable
+    """
+    return callable(fn)
+
+
+def get_url(file: pathlib.Path, start_dir_re: re.Pattern, append_slash: bool, view_fn):
+    """
+    Get the URL for the file
+    """
+    url = getattr(view_fn, "url", "")
+    if not url:
+        url = "" if file.name == "__init__.py" else file.name.replace(".py", "")
+        url = start_dir_re.sub("", f"{file.parent}/{url}").strip("/")
+        url = (url + "/") if append_slash and url != "" else url
+    return url
+
+
+def get_urlname(view_fn, url):
+    """
+    Get the URL name for the view function
+    """
+    urlname = getattr(view_fn, "urlname", "")
+    if not urlname:
+        urlname = DISALLOWED_CHARS.sub("", TO_UNDERSCORES.sub("_", url))
+    return urlname
 
 
 def render_str(source, request, context=None):
